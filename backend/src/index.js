c07274e50f75364b3d43cb123a480b8e12726fd6
Juvenal Miranda <juvenalmirandajr@hotmail.com>
2020-04-05 11:50:12 -0400

    Added funcionality, handling save dev, list devs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose")
 const express = require("express")
+const cors =  require("cors")
 const routes = require("./routes")
 
 const USER_DATABASE = "password"
 const PASSWORD_DATABASE = "password"
 const DATA_BASE = "find_dev_app"
+const app = express()
+
 mongoose.connect(
   `mongodb+srv://${USER_DATABASE}:${PASSWORD_DATABASE}@cluster0-yhvgd.mongodb.net/${DATA_BASE}?retryWrites=true&w=majority`,
   {
@@ -12,8 +15,7 @@ mongoose.connect(
     useUnifiedTopology: true
   }
 )
-
-const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,31 +1,98 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import api from "./services/api"
 import "./global.css"
 import "./App.css"
 import "./Sidebar.css"
 import "./Main.css"
 
 function App() {
+  const [devs, setDevs] = useState([])
+  const [github_username, setGithubUsername] = useState("")
+  const [techs, setTechs] = useState("")
+  const [latitude, setLatitude] = useState("")
+  const [longitude, setLongitude] = useState("")
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords
+        setLatitude(latitude)
+        setLongitude(longitude)
+      },
+      (err) => {
+        console.log(err)
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, [])
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs")
+      setDevs(response.data)
+    }
+    loadDevs()
+  }, [])
+  async function handleAddDev(e) {
+    e.preventDefault()
+    const response = await api.post("/devs", {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    })
+    setDevs([...devs, response.data])
+    setGithubUsername("")
+    setTechs("")
+  }
   return (
     <div id="app">
       <aside>
         <strong>Sign Up</strong>
-        <form>
+        <form onSubmit={handleAddDev}>
           <div className="input-block">
             <label htmlFor="github_username">Github Username</label>
-            <input name="github_username" id="github_username" required />
+            <input
+              name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={(e) => setGithubUsername(e.target.value)}
+            />
           </div>
           <div className="input-block">
             <label htmlFor="techs">Tech Stack</label>
-            <input name="techs" id="techs" required />
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={(e) => setTechs(e.target.value)}
+            />
           </div>
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={(e) => setLatitude(e.target.value)}
+              />
             </div>
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+              <input
+                type="number"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={(e) => setLongitude(e.target.value)}
+              />
             </div>
           </div>
           <button type="submit">Submit</button>
@@ -33,50 +100,21 @@ function App() {
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/12603203?s=460&u=12b35b546b4dac6f986a0484ae43da70ae1ecc77&v=4" alt="Juvenal Miranda Jr."/>
-              <div className="user-info">
-                <strong>Juvenal Miranda Jr.</strong>
-                <span>JavaScript, React.JS, Java</span>
-              </div>
-            </header>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacinia a odio at blandit. Sed mi enim, tristique quis massa eu, aliquet laoreet nisl.</p>
-            <a href="https://github.com/juvenalmirandajr" target="_blank">Link to my profile on Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/12603203?s=460&u=12b35b546b4dac6f986a0484ae43da70ae1ecc77&v=4" alt="Juvenal Miranda Jr."/>
-              <div className="user-info">
-                <strong>Juvenal Miranda Jr.</strong>
-                <span>JavaScript, React.JS, Java</span>
-              </div>
-            </header>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacinia a odio at blandit. Sed mi enim, tristique quis massa eu, aliquet laoreet nisl.</p>
-            <a href="https://github.com/juvenalmirandajr" target="_blank">Link to my profile on Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/12603203?s=460&u=12b35b546b4dac6f986a0484ae43da70ae1ecc77&v=4" alt="Juvenal Miranda Jr."/>
-              <div className="user-info">
-                <strong>Juvenal Miranda Jr.</strong>
-                <span>JavaScript, React.JS, Java</span>
-              </div>
-            </header>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacinia a odio at blandit. Sed mi enim, tristique quis massa eu, aliquet laoreet nisl.</p>
-            <a href="https://github.com/juvenalmirandajr" target="_blank">Link to my profile on Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/12603203?s=460&u=12b35b546b4dac6f986a0484ae43da70ae1ecc77&v=4" alt="Juvenal Miranda Jr."/>
-              <div className="user-info">
-                <strong>Juvenal Miranda Jr.</strong>
-                <span>JavaScript, React.JS, Java</span>
-              </div>
-            </header>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacinia a odio at blandit. Sed mi enim, tristique quis massa eu, aliquet laoreet nisl.</p>
-            <a href="https://github.com/juvenalmirandajr" target="_blank">Link to my profile on Github</a>
-          </li>
+          {devs.map((dev) => (
+            <li className="dev-item" key={dev._id}>
+              <header>
+                <img src={dev.avatar_url} alt={dev.name} />
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(", ")}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${dev.github_username}`} target="_blank">
+                Link to my profile on Github
+              </a>
+            </li>
+          ))}
         </ul>
       </main>
     </div>
